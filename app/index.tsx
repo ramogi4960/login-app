f71df0fb42cdbc5e1726c7fb27b5f6686b6d8c71
Kevin <kramzyvine@icloud.com>
2025-07-17 21:04:22 +0300

    added home screen with onPress animations using react native reanimated

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,78 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Pressable, SafeAreaView, StyleSheet, Text } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
-export default function Index() {
+const image = {uri: "https://w0.peakpx.com/wallpaper/479/900/HD-wallpaper-gradient-purple-blue-gradient-thumbnail.jpg"};
+
+export default function App() {
+  // first, make the Pressable animatable
+  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+  // select what to be animated
+  const opacity = useSharedValue(1);
+  const scale = useSharedValue(1);
+
+  // define how the animation works
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+      transform: [{ scale: scale.value }]
+    };
+  })
+
+  // define how pressing changes the shared values
+  const on_press_in = () => {
+    opacity.value = 0.7;
+    scale.value = 0.9;
+  }
+
+  const on_press_out = () => {
+    opacity.value = 1;
+    scale.value = 1;
+  }
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={image} style={styles.backgroundImage}>
+        <AnimatedPressable style={[
+          styles.animatedPressable,
+          animatedStyles
+        ]}
+        onPressIn={on_press_in}
+        onPressOut={on_press_out}
+        >
+        <Text style={styles.text}>Get Started</Text>
+        </AnimatedPressable>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#25292e',
+    padding: 8,
+  },
+  backgroundImage: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 100
+  },
+  animatedPressable: {
+    height: 100,
+    width: "80%",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: 'center',
+    backgroundColor: "transparent",
+    boxShadow: "0 0 20px #ee8efe"
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30
+  }
+});
